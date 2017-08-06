24b07d2b690a4c6aed31872b75831d7fd51d3f1f
sanumolu2 <30380195+sanumolu2@users.noreply.github.com>
2017-08-06 17:42:46 -0500

    local git

--- src/main/java/com/enterprise/core/OddNumbers.java
+++ src/main/java/com/enterprise/core/OddNumbers.java
@@ -1,13 +1,39 @@
 package com.enterprise.core;
 
 public class OddNumbers {
-	public int findSum(int[] intArray) {
-		return 0;
-	}
-	
-	public float findAverage(int[] intArray) {
-		return 0;
-	
+	public int findSum( int[] array){
+		 int sum1=0;
+		for(int i=0; i<array.length; i++)
+		{
+			if (array[i] %2 != 0){
+		 
+			sum1+=array[i];
+				
+			}
+		
+		}
+		return sum1;
+		
+		}
+	public float findAverage(int[] arr1)
+	{
+		
+		float avg=0;
+		for(int i=0; i<arr1.length;i++)
+		{
+			if ( arr1[i] !=0){
+				avg+=arr1[i];
+			}
+			
+			//if ( arr1[i] %2 !=0){
+				//avg+=arr1[i];
+			//}
+		}
+		
+		avg= avg/arr1.length;
+		
+		return avg;
+		
 	}
 
 }
--- src/main/java/com/enterprise/core/TripleDouble.java
+++ src/main/java/com/enterprise/core/TripleDouble.java
@@ -6,18 +6,59 @@ public class TripleDouble {
 
 	public int find(int first, int second) {
 		// convert to strings
+
+		String s1 = Integer.toString(first);
+		char a[] = s1.toCharArray();
+
+		String s2 = Integer.toString(second);
+		char b[] = s2.toCharArray();
+
 		// loop thru first string chars to find 3 consecutive same char
-			// if found 3 consecutive same char then
-				// if the same char is repeated two times in second string then
-				// return 1
-				//end if
-			//end if
-		//end loop
-		//return 0
+		int count = 1;
+		int count2 = 0;
+		int repeatDigit = 0;
+		int repeatDigit2 = 0;
+		int result = 0;
+// consecutive search for 3 digits in a 
+		for (int i = 0; i < a.length; i++) {
+			if (i < a.length - 2)
+			{
+
+				if (a[i] == a[i + 1])
+				{
+
+					count++;
+				
+				if (a[i + 1] == a[i + 2]) 
+				{
+					count++;
+				}
+					repeatDigit = a[i];
+				}
+				
+			}
+		}
+// consecutive search in b
+		for (int j = 0; j < b.length; j++) {
+			if (j < b.length - 1) {
+
+				if (b[j] == b[j + 1]) 
+				{
+					count2++;
+					repeatDigit2 = b[j];
+
+				}
+			}
+		}
+// comparing the digit and count
+		if (repeatDigit == repeatDigit2 && count == count2 + 1) {
+			
+			result = 1;
+		}
+
 		
-		//this can be done with regular expression too
-		
-		return 0;
+
+		return result;
 
 	}
 
